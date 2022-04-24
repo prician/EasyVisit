beb441bb19c82faa0598ba22bfdc444254f8dbe1
prician <1258106285@qq.com>
2022-04-24 19:35:11 +0800

    增加二级界面

--- Easyvisit/Classes/Business/FirstTab/Controller/MedicineViewController.swift
+++ Easyvisit/Classes/Business/FirstTab/Controller/MedicineViewController.swift
@@ -9,12 +9,74 @@ import UIKit
 
 class MedicineViewController: UIViewController {
 
+    let medicineCellID = "medicineCellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configUI()
+        self.view.backgroundColor = UIColor(red: 0.973, green: 0.973, blue: 0.973, alpha: 1)
     }
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.showsHorizontalScrollIndicator = false // 隐藏滑动条
+        collectionView.alwaysBounceVertical = false
+        collectionView.showsVerticalScrollIndicator = false
+        
+        collectionView.register(MedicineCollectionViewCell.self, forCellWithReuseIdentifier: medicineCellID)
+        return collectionView
+    }()
+    
+    
+    func configUI() {
+        self.view.addSubview(collectionView)
+       
+        collectionView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(0.fh)
+            make.left.equalToSuperview().offset(20.fw)
+            make.right.equalToSuperview().offset(-20.fw)
+            make.height.equalTo(screenHeight)
+        }
+        
+        
+    }
+
+}
+
+extension  MedicineViewController:  UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: medicineCellID, for: indexPath) as! MedicineCollectionViewCell
+        cell.layer.cornerRadius = 20
+        return cell
+    }
+
+}
     
 
 
+extension MedicineViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: Int(screenWidth) - 40, height: 135.fh)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: CGFloat(0.fh), left: CGFloat(0.fw), bottom: CGFloat(20.fh), right: CGFloat(0.fw))
+    }
 }
+
+
